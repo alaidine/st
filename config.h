5f64391a7fa34de17ac4ac9837abd283a36d99a5
aalaaiddine alimlinde <aalaaiddine.alimlinde@cg974.fr>
2024-07-24 12:54:55 +0400

    use catppuccin as default colorscheme

--- config.h
+++ config.h
@@ -113,28 +113,28 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#45475a", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#f38ba8",
+	"#a6e3a1",
+	"#f9e2af",
+	"#89b4fa",
+	"#cba6f7",
+	"#94e2d5",
+	"#bac2de",
+	"#585b70",
+	"#f38ba8",
+	"#a6e3a1",
+	"#f9e2af",
+	"#89b4fa",
+	"#cba6f7",
+	"#94e2d5",
+	"#a6adc8",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+	"#000000", /* 258 -> bg */
+	"#cdd6f4", /* 259 -> fg */
 };
 
 
